2d5ebd854c7215a3baa4d1c5ca422b4f8bda6426
DazzledTurtle22 <110479265+DazzledTurtle22@users.noreply.github.com>
2022-08-02 16:06:52 +0100

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n";
+  cout << "I'm learning C++! \n\n";
+  cout << "I like coding." << endl;
+  cout << "It is very fun. \t";
+  cout << "Programming is very interesting. \\";
+  cout << "It's my \"hobby\".";
+  return 0;
+}
+
+// This is a single line comment.
+/* This is a multi-line comment. 
+cout << "" allows you to print strings. All functions must end in a ;
+\n new line 
+\n\n blank line 
+<< endl new line 
+\t horizontal tab 
+\\ inserts a \ 
+\"\" inserts speech marks around a word when being printed */
---
+++ variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myNum = 15;
+    cout << myNum << endl;
+    char yourLetter;
+    yourLetter = 'B';
+    cout << yourLetter << endl;
+    string yourWord;
+    yourWord = "No";
+    cout << yourWord << endl;
+    int myAge = 14;
+    cout << "I am " << myAge << " years old. \t";
+    int x = 5;
+    int y = 10;
+    int sum = x + y;
+    cout << sum;
+
+}
